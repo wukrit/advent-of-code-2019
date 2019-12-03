f0f5432bfaa9814ea15e59cafbf9fb64b58d4472
Wukrit <sukritwalia@gmail.com>
2019-12-02 19:31:26 -0500

    implemented function to read input.txt

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,11 @@
-console.log(input)
+const fs = require("fs");
+
+const splitData = (data) => {
+  return data.split('\n').map(entry => parseInt(entry))
+}
+
+fs.readFile("input.txt", "utf8", (error, data) => {
+  if (error) throw error;
+  // Solution goes here
+  console.log(splitData(data));
+});
