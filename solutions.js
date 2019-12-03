c230ff59dce7940e3350fafcad5e9975ec6b95a2
Wukrit <sukritwalia@gmail.com>
2019-12-02 23:22:32 -0500

    created file to execute all completed challenges

---
+++ solutions.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const { exec } = require("child_process")
+
+const formatNumber = num => num < 10 ? "0" + num : num
+
+const runFile = day => {
+  const path = day + "/index.js"
+  if (fs.existsSync(path)) {
+    console.log("--- Day " + day + " ---")
+    exec("node " + path, (error, stdout, stderr) => {
+      console.log(stdout);
+      console.error(stderr);
+    })
+  }
+}
+
+for (let i = 1; i <= 25; i++)
+  runFile(formatNumber(i));
