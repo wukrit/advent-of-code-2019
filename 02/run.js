5ecbabb3419b0b2d7c1d4b9d3a76799fc404faee
Wukrit <sukritwalia@gmail.com>
2019-12-03 17:37:15 -0500

    Part 1 of 02 completed

--- 02/run.js
+++ 02/run.js
@@ -7,9 +7,53 @@ const parseInput = data => {
   return arr.map(value => parseInt(value));
 };
 
+// Restore "1202 Program Alarm"
+const restore1202 = input => {
+  input[1] = 12;
+  input[2] = 2;
+};
+
+// Process 4 Part Command
+const processOp = ([opCode, x, y, location], input) => {
+  let value = 0
+  if (opCode == 1) value = input[x] + input[y];
+  if (opCode == 2) value = input[x] * input[y];
+  return [value, location];
+};
+
+// Modify Corresponding Value of Input
+const runOp = ([value, location], input) => {
+  if (value === "End") return value;
+  input[location] = value;
+  return input;
+}
+
+// Create list of op commands
+const commands = input => {
+  const commandArr = []
+  const chunk = input => {
+    commandArr.push(input.slice(0, 4));
+    if (input.slice(4).length >= 4 && input[4] != 99) chunk(input.slice(4));
+    return;
+  };
+  chunk(input)
+  return commandArr
+}
+
+// Perform operations on Input
+const performOps = (input, commands) => {
+  for(entry of commands) {
+    const [value, location] = processOp(entry, input);
+    input[location] = value;
+  }
+  return input
+}
+
 fs.readFile(path.join(__dirname, "input.txt"), "utf8", (error, data) => {
   if (error) throw error;
 
   const input = parseInput(data);
-  console.log(input);
+  restore1202(input);
+  const output = performOps(input, commands(input))
+  console.log("Part 1: ", output[0])
 });
