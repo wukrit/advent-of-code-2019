373f5bf49d0f8df13b0938bd1213fda1606f8d2a
Wukrit <sukritwalia@gmail.com>
2019-12-02 23:44:39 -0500

    prettified code

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const fs = require("fs");
-const { exec } = require("child_process")
+const { exec } = require("child_process");
 
-const formatNumber = num => num < 10 ? "0" + num : num
+const formatNumber = num => (num < 10 ? "0" + num : num);
 
 const runFile = day => {
-  const path = day + "/run.js"
+  const path = day + "/run.js";
   if (fs.existsSync(path)) {
-    console.log("--- Day " + day + " ---")
+    console.log("--- Day " + day + " ---");
     exec("node " + path, (error, stdout, stderr) => {
       console.log(stdout);
       console.error(stderr);
-    })
+    });
   }
-}
+};
 
-for (let i = 1; i <= 25; i++)
-  runFile(formatNumber(i));
+for (let i = 1; i <= 25; i++) runFile(formatNumber(i));
