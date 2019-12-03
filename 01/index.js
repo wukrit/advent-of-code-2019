88beaeee2ec296b9c3b00cb1ca79ac486deb4e5d
Wukrit <sukritwalia@gmail.com>
2019-12-02 20:30:19 -0500

    01-done

--- 01/index.js
+++ 01/index.js
@@ -9,9 +9,14 @@ const parseInput = data => {
   return arr;
 };
 
+const calcFuel = mass => {
+  return Math.floor(mass / 3.0) - 2;
+};
+
 fs.readFile("input.txt", "utf8", (error, data) => {
   if (error) throw error;
-  // Solution goes here
+
   const input = parseInput(data);
-  for (let i = 0; i < input.length; i++) console.log(input[i]);
+  const sum = input.reduce((sum, value) => (sum += calcFuel(value)), 0);
+  console.log(sum);
 });
