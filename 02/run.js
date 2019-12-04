1e156f551219de8419beee1e19c3cb24748dc81c
Wukrit <sukritwalia@gmail.com>
2019-12-03 22:57:56 -0500

    Added findCombo function for part 2

--- 02/run.js
+++ 02/run.js
@@ -9,10 +9,10 @@ const parseInput = data => {
 
 // Set Noun/Verb Combo
 const setNV = (input, noun, verb) => {
-  const newArr = [...input]
+  const newArr = [...input];
   newArr[1] = noun;
   newArr[2] = verb;
-  return newArr
+  return newArr;
 };
 
 // Process 4 Part Command
@@ -40,7 +40,7 @@ const commands = input => {
 
 // Perform operations on Input
 const performOps = (input, commands) => {
-  const newArr = [...input]
+  const newArr = [...input];
   for (entry of commands) {
     const [value, location] = processOp(entry, newArr);
     newArr[location] = value;
@@ -50,15 +50,24 @@ const performOps = (input, commands) => {
 
 // Part 2, Test Noun/Verb Combos
 const findCombo = (input, target) => {
-
-}
+  for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+      const inputArr = setNV(input, noun, verb);
+      const output = performOps(inputArr, commands(inputArr));
+      if (output[0] === target) return 100 * noun + verb;
+    }
+  }
+};
 
 fs.readFile(path.join(__dirname, "input.txt"), "utf8", (error, data) => {
   if (error) throw error;
 
   const input = parseInput(data);
+
   const inputPart1 = setNV(input, 12, 2);
   const outputPart1 = performOps(inputPart1, commands(inputPart1));
-  console.log(input)
+  const outPutPart2 = findCombo(input, 19690720);
+
   console.log("Part 1: ", outputPart1[0]);
+  console.log("Part 2: ", outPutPart2);
 });
