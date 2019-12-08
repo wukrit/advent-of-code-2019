991c349355d4472d89a4b99b2e89bf5e4eb35979
Wukrit <sukritwalia@gmail.com>
2019-12-08 17:52:32 -0500

    seperated wires 1 and 2

--- 03/run.js
+++ 03/run.js
@@ -3,13 +3,14 @@ const path = require("path");
 
 // Process text file and return array
 const parseInput = data => {
-  return data.split(",");;
+  const [wire1, wire2] = data.split("\n");
+  return [wire1.split(","), wire2.split(",")]
 };
 
 fs.readFile(path.join(__dirname, "input.txt"), "utf8", (error, data) => {
   if (error) throw error;
 
-  const input = parseInput(data);
+  const [wire1, wire2] = parseInput(data);
 
-  console.log(input);
+  console.log(wire1, "\n", wire2);
 });
