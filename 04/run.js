8bf90c8a6082641ca6fc9a4ba401014c48e65c8d
Wukrit <sukritwalia@gmail.com>
2019-12-08 23:33:51 -0500

    implemented function to check presence of doubles

--- 04/run.js
+++ 04/run.js
@@ -1,3 +1,14 @@
 // Given input range
 const range = [197487, 673251]
 
+// Check for presence of doubles
+const double = num => {
+  const password = num.toString().split("");
+  let double = false;
+  for (let i = 1; i < password.length; i++)
+    if (password[i] == password[i - 1]) double = true;
+  return double
+}
+
+console.log(double(102312)); // => false
+console.log(double(123441)); // => true
