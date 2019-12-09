badb164bbb7225296d0fd4ad3e1cc2191feb9027
Wukrit <sukritwalia@gmail.com>
2019-12-08 23:41:25 -0500

    part 1 completed day 03

--- 04/run.js
+++ 04/run.js
@@ -10,5 +10,21 @@ const double = num => {
   return double
 }
 
-console.log(double(102312)); // => false
-console.log(double(123441)); // => true
+// Check to see if numbers only rise
+const rising = num => {
+  const password = num.toString().split("");
+  let rising = true;
+  for (let i = 1; i < password.length; i++)
+    if (password[i] < password[i - 1]) rising = false;
+  return rising;
+}
+
+// Find all passwords that match critera
+const findCombinations = ([start, end]) => {
+  const combos = []
+  for (let i = start; i <= end; i++)
+    if (double(i) && rising(i)) combos.push(i);
+  return combos
+}
+
+console.log("Part 1: ", findCombinations(range).length)
